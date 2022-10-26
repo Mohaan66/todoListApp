1565166a980cd2d4d8c5491dcf364af37babb753
Mohamed Abdi <mohaan669@gmail.com>
2022-10-26 19:55:07 +0100

    Updates

---
+++ precache-manifest.9e759a074f129f96d329367881a395dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5767a6c610e0c3ac88a",
+    "url": "/todoListApp/static/css/main.3929decf.chunk.css"
+  },
+  {
+    "revision": "a5767a6c610e0c3ac88a",
+    "url": "/todoListApp/static/js/main.a5767a6c.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/todoListApp/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "d92e498dc6a7520e608d",
+    "url": "/todoListApp/static/js/2.d92e498d.chunk.js"
+  },
+  {
+    "revision": "81fb55a3e14479c87f31",
+    "url": "/todoListApp/static/js/runtime~main.81fb55a3.js"
+  },
+  {
+    "revision": "f8d206f9c31fe26876efe8bb715aa92e",
+    "url": "/todoListApp/index.html"
+  }
+];
